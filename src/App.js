e7f1741fb882d8cf7cb2a2311116101a570d2fbc
Animesh Raj <animesh080808@gmail.com>
2023-05-03 07:47:57 +0530

    feat : learned UseState
    Signed-off-by: Animesh Raj <animesh080808@gmail.com>

--- src/App.js
+++ src/App.js
@@ -2,15 +2,18 @@ import './App.css';
 import React, { useState } from 'react';
 
 function App() {
-    const email = '213';
-    const setEmail = '213';
+    // const [email , setEmail] = useState('');
+    const emailState = useState('');
+    const email = emailState[0];
+    const setEmail = emailState[1];
 
     function handleChange(e) {
         setEmail(e.target.value);
     }
+
     return (
         <div className="App">
-            <input value={email} onChange={handleChange} />
+            <input v alue={email} onChange={handleChange} />
             <p> Email : {email}</p>
         </div>
     );
